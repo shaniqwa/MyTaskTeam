a05d38e9467e1f19e7fab4de387af73d82ba0229
Shani <s.almog88@gmail.com>
2016-02-11 19:46:17 +0200

    fix ReportTask conflict

--- app/src/main/java/com/mobile/shenkar/shani/mytaskteam/ReportTask.java
+++ app/src/main/java/com/mobile/shenkar/shani/mytaskteam/ReportTask.java
@@ -2,29 +2,35 @@ package com.mobile.shenkar.shani.mytaskteam;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by Shani on 1/18/16.
+
  */
 public class ReportTask extends AppCompatActivity {
-        private JSONObject task;
-        protected TextView cat;
-        protected TextView des;
-        protected TextView assignee;
-        protected TextView location;
-        protected TextView dueDate;
-        protected TextView status;
+    private JSONObject task;
+    protected TextView cat;
+    protected TextView des;
+    protected TextView assignee;
+    protected TextView location;
+    protected TextView dueDate;
+    protected TextView status;
+    protected TextView priority;
 
-        private RadioButton accept;
-        private RadioButton accept_waiting;
-        private RadioButton accept_in_process;
-        private RadioButton accept_done;
-        private RadioButton reject;
+    private RadioButton accept;
+    private RadioButton accept_waiting;
+    private RadioButton accept_in_process;
+    private RadioButton accept_done;
+    private RadioButton reject;
+    private RadioGroup radioGroupStatus;
+    private Button save;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,15 +55,36 @@ public class ReportTask extends AppCompatActivity {
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         cat = (TextView) findViewById(R.id.textCat);
         des = (TextView) findViewById(R.id.textDes);
+        location=(TextView) findViewById(R.id.textLocation);
+        priority=(TextView) findViewById(R.id.textPriority);
+        dueDate=(TextView) findViewById(R.id.textDueDate);
         assignee = (TextView) findViewById(R.id.textAssignee);
-        assignee = (TextView) findViewById(R.id.textAssignee);
+
 
         // radio button links to layout
         accept = (RadioButton)findViewById(R.id.accept);
         reject = (RadioButton)findViewById(R.id.reject);
+        accept_waiting = (RadioButton)findViewById(R.id.waiting);
+        accept_in_process = (RadioButton)findViewById(R.id.in_process);
+        accept_done = (RadioButton)findViewById(R.id.done);
+
+
 
         try {
             cat.setText(task.getString("cat"));
+            des.setText(task.getString("des"));
+            location.setText(task.getString("location"));
+            assignee.setText(task.getString("assignee"));
+
+            dueDate.setText(task.getString("dueTime"));
+
+            if(task.getString("priority").compareTo("1") == 0 ){
+                priority.setText("Low");
+            }else if (task.getString("priority").compareTo("2") == 0 ){
+                priority.setText("Med");
+            }else if (task.getString("priority").compareTo("3") == 0){
+                priority.setText("High");
+            }
             setRadioForStatus(task.getString("status"));
         } catch (JSONException e) {
             cat.setText("Error");
@@ -65,39 +92,44 @@ public class ReportTask extends AppCompatActivity {
     }
 
     void setRadioForStatus(String p) {
-        // set all to false
-        reject.setSelected(false);
-        accept.setSelected(false);
-        accept_waiting.setSelected(false);
-        accept_in_process.setSelected(false);
-        accept_done.setSelected(false);
+        try {
+            // set all to false
+            reject.setSelected(false);
+            accept.setSelected(false);
+            accept_waiting.setSelected(false);
+            accept_in_process.setSelected(false);
+            accept_done.setSelected(false);
 
 
-        // set the proper one to true
-        if(p.compareTo("4")==1) {
-            // rej
-            reject.setSelected(true);
+            // set the proper one to true
+            if(p.compareTo("4")==1) {
+                // rej
+                reject.setSelected(true);
+            }
+            else {
+                // acc
+                accept.setSelected(true);
+
+                if(p.compareTo("0") == 1) {
+                    // waiting
+                    accept_waiting.setSelected(true);
+
+                }
+                else if(p.compareTo("2") == 1) {
+                    // in process
+                    accept_in_process.setSelected(true);
+
+                }
+                else if(p.compareTo("3") == 1) {
+                    // done
+                    accept_done.setSelected(true);
+                }
+
+            }
         }
-        else {
-            // acc
-            accept.setSelected(true);
-
-            if(p.compareTo("0") == 1) {
-                // waiting
-                accept_waiting.setSelected(true);
-
-            }
-            else if(p.compareTo("2") == 1) {
-                // in process
-                accept_in_process.setSelected(true);
-
-            }
-            else if(p.compareTo("3") == 1) {
-                // done
-                accept_done.setSelected(true);
-            }
-
+        catch (Exception ex) {
+            Log.e("error", ex.toString());
         }
+
     }
-
 }
